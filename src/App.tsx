384122fdc45b4af52fa4d55c03a4568b84514ce3
Pooja-Patel797 <66893794+Pooja-Patel797@users.noreply.github.com>
2021-09-25 15:51:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/header";
 import { Home } from "./components/home";
 import { AboutMe } from "./components/aboutMe";
 import { Blogs } from "./components/blogs";
+import { Certificates } from "./components/certifications";
 import { Skills } from "./components/skills";
 import { Projects } from "./components/projects";
 import { Footer } from "./components/footer";
@@ -17,6 +18,7 @@ export const App: React.FC = () => {
       <AboutMe />
       <Skills />
       <Projects />
+      <Certificates />
       <Blogs />
       <Footer />
     </div>
