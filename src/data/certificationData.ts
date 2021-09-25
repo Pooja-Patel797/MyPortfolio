840ae76a5f8b0894384b05d249812a4b45e1f555
Pooja-Patel797 <66893794+Pooja-Patel797@users.noreply.github.com>
2021-09-25 15:40:02 +0530

    Create certicateData.ts

---
+++ src/data/certificationData.ts
@@ -0,0 +1,52 @@
+export const certdata = [
+ {
+    id: 1,
+    heading: "E&Y Certificate",
+    text:
+      "Internship Certificate",
+    image: reactlogo,
+    url:
+      "https://drive.google.com/file/d/1uaBC7JhtQt-1Hd-KUf4wv_Nm9JuSQRf-/view?usp=sharing",
+    description: "Internship Certificate...",
+  },
+  {
+    id: 2,
+    heading: "Docker Essentials: A Developer Introduction",
+    text:
+      "hands-on labs that demonstrate how to use containers for your applications.Brief introduction to running containers in production with the and how to solve problems of advanced orchestration",
+    image: hooks,
+    url: "https://courses.cognitiveclass.ai/certificates/60f7520df0ca4785aa8b4612b658c777",
+    description: "hands-on labs that demonstrate how to use containers for your applications.Brief introduction to running containers in production...",
+  },
+  {
+    id: 3,
+    heading: "Python MegaCourse",
+    text:
+      "Build real-life desktop programs with graphical user interfaces,  automate routine tasks, control the computer webcam, develop interactive web applications using Flask and Django, scrape and visualize data,",
+    image: seo,
+    url:
+      "https://udemy-certificate.s3.amazonaws.com/pdf/UC-CUMMOTWM.pdf",
+    description: "Build real-life desktop programs with graphical user interfaces,  automate routine tasks...",
+  },
+  {
+    id: 4,
+    heading: "Git Complete",
+    text:
+      "Git Complete: The definitive, step-by-step guide to Git",
+    image: reactlogo,
+    url:
+      "https://udemy-certificate.s3.amazonaws.com/pdf/UC-WY793FXX.pdf",
+    description: "Git Complete: The definitive, step-by-step guide to Git",
+  },
+ {
+    id: 5,
+    heading: "PRO JAVA",
+    text:
+      "Git complete",
+    image: reactlogo,
+    url:
+      "",
+    description: "Deep knowledege of Core Java.Frameworks-Spring(Core+MVC), Hibernate,Maven...",
+  },
+
+];
