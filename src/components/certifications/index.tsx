c1e7fc3d67e1dedc8443adc96af29acecac9442f
Pooja-Patel797 <66893794+Pooja-Patel797@users.noreply.github.com>
2021-09-25 15:46:53 +0530

    Create index.ts

---
+++ src/components/certifications/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import Aos from "aos";
+import { certdata } from "../../data";
+import { HeaderSection } from "../common";
+import "./style.css";
+
+export const Certificates: React.FC = () => {
+  const [header] = React.useState({
+    subHeader: "My Certifications",
+    text: "Keep Learning...",
+  });
+
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div id="blogs" className="blogs">
+      <div data-aos="fade-up" className="blogs__container">
+        <div className="blogs__header">
+          <HeaderSection subHeader={header.subHeader} text={header.text} />
+
+          <div className="container__flex base">
+            {certdata.map((info, key) => (
+              <div
+                key={key}
+                data-aos="flip-left"
+                className="container__flex__box base"
+              >
+                <div className="blogs__box">
+                  <br />
+                  <div className="blogs__box-header"> {info.heading}</div>
+
+                  <img
+                    className="blogs__box-image"
+                    src={info.image}
+                    alt={info.description}
+                  />
+
+                  <a href={info.url}>
+                    <div className="blogs__box-p">{info.text}</div>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
