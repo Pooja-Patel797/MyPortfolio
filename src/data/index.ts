0fd638aa2c55769ce10d0ff2092598b8ba4f1014
Pooja-Patel797 <66893794+Pooja-Patel797@users.noreply.github.com>
2021-09-25 15:43:04 +0530

    Updated index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,4 @@ export { skilldata } from "./skillData";
 export { introdata, intro } from "./introData";
 export { projectdata } from "./projectData";
 export { blogdata } from "./blogData";
+export { certdata } from "./certificationData";
