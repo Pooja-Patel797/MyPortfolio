22978f709a124af4265992faf6b0ee63ee4a7816
Pooja-Patel797 <66893794+Pooja-Patel797@users.noreply.github.com>
2021-09-25 21:31:15 +0530

    Updated introData.ts

--- src/data/introData.ts
+++ src/data/introData.ts
@@ -1,11 +1,11 @@
 export const intro = {
   "about__info-p1":
-    " I have completed my degree in Information Technology from Devi Ahilya University, Indore and passed out in the year 2020.I have also done 8 months of internship in one of big four accounting firms Ernst & Young GDS.\n\
+    " I have completed my degree in Information Technology from Devi Ahilya University, Indore and passed out in the year 2021.I have also done 8 months of internship in one of big four accounting firms Ernst & Young GDS.\n\
     I have keen interest in coding and web application development.",
 
   "about__info-p2":
-    " Apart from coding i am fond of doing meditation,yoga and love\n\
-    music.I also like reading blogs and surfing inspirational and technical talks on internet.",
+    " Apart from coding I am fond of doing meditation,yoga and love\n\
+    music.I like spending time with family and friends and love to explore natural places.",
 };
 export const introdata = [
   { id: 1, title: "Name:", text: "Pooja Patel" },
