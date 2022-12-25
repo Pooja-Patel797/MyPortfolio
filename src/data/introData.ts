4be4a23f9541757c7faf8155dc31d2c9a4a8fc2a
Pooja-Patel797 <66893794+Pooja-Patel797@users.noreply.github.com>
2022-12-25 15:52:26 +0530

    Removed escape character

--- src/data/introData.ts
+++ src/data/introData.ts
@@ -2,7 +2,7 @@ export const intro = {
   "about__info-p1":
     " I have completed my degree in Information Technology from Devi Ahilya University, Indore and passed out in the year 2022.",
   
-   "about__info-p2": " Experience :Oracle (OCI)[Aug 2022 -present]: Site Reliability Engineer\n\ | Persistent Systems Limited [Jan 2021-July 2022] : Software Engineer | Ernst&Young GDS [Aug 2019 - Mar 2020]: Intern \n\ ",
+   "about__info-p2": " Experience :Oracle (OCI)[Aug 2022 -present]: Site Reliability Engineer | Persistent Systems Limited [Jan 2021-July 2022] : Software Engineer | Ernst&Young GDS [Aug 2019 - Mar 2020]: Intern",
 
     "about__info-p3":
     " Apart from coding I am fond of doing meditation,yoga and love\n\
