2c8af2c59d2b1352cd18903b9094d65bc7af7d38
Pooja-Patel797 <66893794+Pooja-Patel797@users.noreply.github.com>
2022-12-25 15:50:15 +0530

    UpdatedIntro

--- src/data/introData.ts
+++ src/data/introData.ts
@@ -1,12 +1,10 @@
 export const intro = {
   "about__info-p1":
-    " I have completed my degree in Information Technology from Devi Ahilya University, Indore and passed out in the year 2022.\n\
-      Experience :\n\ \n\
-      Oracle (OCI)[Aug 2022 -present]                 : Site Reliability Engineer \n\ \n\
-      Persistent Systems Limited [Jan 2021-July 2022] : Software Engineer \n\ \n\
-      Ernst&Young GDS [Aug 2019 - Mar 2020]           : Intern \n\ \n\
-     ",
-    "about__info-p2":
+    " I have completed my degree in Information Technology from Devi Ahilya University, Indore and passed out in the year 2022.",
+  
+   "about__info-p2": " Experience :Oracle (OCI)[Aug 2022 -present]: Site Reliability Engineer\n\ | Persistent Systems Limited [Jan 2021-July 2022] : Software Engineer | Ernst&Young GDS [Aug 2019 - Mar 2020]: Intern \n\ ",
+
+    "about__info-p3":
     " Apart from coding I am fond of doing meditation,yoga and love\n\
     music.I like spending time with family and friends and love to explore and visit new places.",
 };
